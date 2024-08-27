a3c05417aca7270e3fae95f6ff632e079edc5bda
Arnau Quera-Bofarull <arnauq@protonmail.com>
2024-08-27 11:06:14 +0200

    fixed tests

--- src/GameOfLife.jl
+++ src/GameOfLife.jl
@@ -1,6 +1,17 @@
-# implementation from https://github.com/gaurav-arya/StochasticAD.jl/blob/main/tutorials/game_of_life/core.jl
 export GameOfLifeParams
 
+"""
+    GameOfLifeParams{T, Q, S}
+
+Parameters for the Game of Life simulation.
+
+# Fields
+- `n_timesteps::Int64`: Number of timesteps to simulate
+- `board_length::Int64`: Length of one side of the square board
+- `probs::Vector{T}`: Probabilities for cell state transitions
+- `initial_prob::Vector{Q}`: Initial probabilities for cell states
+- `discrete_sampler::S`: Sampler for discrete random variables
+"""
 struct GameOfLifeParams{T, Q, S}
     n_timesteps::Int64
     board_length::Int64
@@ -10,6 +21,20 @@ struct GameOfLifeParams{T, Q, S}
 end
 @functor GameOfLifeParams (initial_prob, probs,)
 
+"""
+    select_prob(probs, index::T) where {T}
+
+Select a probability from `probs` based on the given `index`.
+
+This function interpolates between discrete probability values when `index` is not an integer.
+
+# Arguments
+- `probs`: Vector of probabilities
+- `index::T`: Index to select probability (can be a float)
+
+# Returns
+- Selected probability value
+"""
 function select_prob(probs, index::T) where {T}
     # since index is a float we need to express the indexing operation all_probs[index] as 
     # a sum of masks products.
@@ -29,6 +54,21 @@ function select_prob(probs, index::T) where {T}
     return res
 end
 
+"""
+    update_state!(bernoulli_sampler, probs, N, board, board_old)
+
+Update the state of the Game of Life board for one timestep.
+
+# Arguments
+- `bernoulli_sampler`: Sampler for Bernoulli random variables
+- `probs`: Vector of probabilities for cell state transitions
+- `N`: Half the length of one side of the board
+- `board`: Current state of the board (will be modified)
+- `board_old`: Previous state of the board
+
+# Returns
+- Updated `board`
+"""
 function update_state!(bernoulli_sampler, probs, N, board, board_old)
     for i in (-N):N
         for j in (-N):N
@@ -43,6 +83,19 @@ function update_state!(bernoulli_sampler, probs, N, board, board_old)
     return board
 end
 
+"""
+    abm_step(params::GameOfLifeParams, x, t)
+
+Perform one step of the Game of Life simulation.
+
+# Arguments
+- `params::GameOfLifeParams`: Parameters for the simulation
+- `x`: Current state of the board
+- `t`: Current timestep (not used in this function)
+
+# Returns
+- New state of the board after one step
+"""
 function abm_step(params::GameOfLifeParams, x, t)
     new_board = copy(x)
     update_state!(
@@ -50,6 +103,17 @@ function abm_step(params::GameOfLifeParams, x, t)
     return new_board
 end
 
+"""
+    abm_run(params::GameOfLifeParams{T, Q, S}) where {T, Q, S}
+
+Run the full Game of Life simulation for the specified number of timesteps.
+
+# Arguments
+- `params::GameOfLifeParams{T, Q, S}`: Parameters for the simulation
+
+# Returns
+- Vector of board states at each timestep, where each state is represented by the sum of all cell values
+"""
 function abm_run(params::GameOfLifeParams{T, Q, S}) where {T, Q, S}
     N = params.board_length
     board = OffsetArray(zeros(Q, 2 * N + 3, 2 * N + 3), (-(N + 1)):(N + 1),
--- test/sir_test.jl
+++ test/sir_test.jl
@@ -8,6 +8,7 @@ using Test
     number_per_venue = [1, 2, 3, 4]
     graph = DiffABM.generate_random_world_graph(
         n_agents, venues, fraction_population_per_venue, number_per_venue)
+    graph_generator() = graph
     initial_infected = [0.1]
     gamma = [0.05]
     betas = [0.5, 0.2, 0.3, 0.1]
@@ -17,7 +18,7 @@ using Test
     policies = Policies()
     for discrete_sampler in [GS(0.1), SAD(), ST(), SM()]
         params = SIRParams(
-            graph, initial_infected, betas, venues, gamma, delta_t, n_timesteps,
+            graph_generator, initial_infected, betas, venues, gamma, delta_t, n_timesteps,
             discrete_sampler, infection_type, policies)
         delta_I_ts = abm_run(params)
         @test length(delta_I_ts) == n_timesteps
